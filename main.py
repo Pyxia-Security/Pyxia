472884afe5140dd988ba3c8f065b2afc3b91c4d5
Seb J <pyxia-security@proton.me>
2024-07-12 22:11:37 +1200

    Version 3 - 18-06-2024

--- main.py
+++ main.py
@@ -1,17 +1,91 @@
 #!/usr/bin/env python3
-
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, session
+from testdatabase import *
+import os, flask_login
+from flask_login import current_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(32).hex()
+login_manager = flask_login.LoginManager()
 
-@app.route("/")
+login_manager.init_app(app)
+
+class User(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(username):
+    if username not in users:
+        return
+    user = User()
+    user.id = username
+    return user
+
+@login_manager.request_loader
+def request_loader(request):
+    username = request.form.get('username')
+    if username not in users:
+        return
+    
+    user = User()
+    user.id = username
+    return user
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 
-@app.route("/login")
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        if current_user.is_authenticated == True:
+            return redirect('/')
+        return render_template("login.html")
+    username = request.form.get('username')
+    if username in users and request.form.get('password') == users[username]['password']:
+        user = User()
+        user.id = username
+        flask_login.login_user(user)
+        return redirect('/')
+    return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return redirect('/')
+
+@app.route('/signup')
+def signup():
+    if current_user.is_authenticated == True:
+            return redirect('/') 
+    return render_template("signup.html")
+
+@login_manager.unauthorized_handler
+def login_unauthorized_handler():
+    return render_template("error/401.html"), 401
+
+@app.errorhandler(401)
+def unauthorized_handler(e):
+    return render_template("error/401.html"), 401
+
+@app.errorhandler(403)
+def forbidden_handler(e):
+    return render_template("error/403.html"), 403
+
+@app.errorhandler(404)
+def notfound_handler(e):
+    return render_template("error/404.html"), 404
+
+@app.errorhandler(410)
+def gone_handler(e):
+    return render_template("error/410.html"), 410
+
+@app.errorhandler(500)
+def intserverror_handler(e):
+    return render_template("error/500.html"), 500
+
 
 if __name__ == "__main__":
-    # deepcode ignore RunWithDebugTrue: disable in final project, as is needed.
+    # deepcode ignore RunWithDebugTrue: disable debug in final project, as is for testing/debugging purposes.
     app.run(host='0.0.0.0', port=80, debug=True)
---
+++ testdatabase.py
@@ -0,0 +1,2 @@
+# deepcode ignore NoHardcodedPasswords: for testing purposes, not in final build.
+users = {'seb': {'password': 'password1234'}}
